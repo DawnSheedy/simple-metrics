d3e3ca3bcd6c0c58efa89f7b53c48523ff7f68b9
DawnSheedy <dawn@dawnsheedy.com>
2024-08-06 18:00:56 -0700

    fix invalid connection arg for postgres

--- app/database.py
+++ app/database.py
@@ -3,10 +3,14 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-SQLALCHEMY_DATABASE_URL = os.getenv('POSTGRES_CONNECTION_STRING', 'sqlite:///./sql_app.db')
+SQLALCHEMY_DATABASE_URL = os.getenv(
+    "POSTGRES_CONNECTION_STRING", "sqlite:///./sql_app.db"
+)
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+engine = (
+    create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+    if SQLALCHEMY_DATABASE_URL.startswith("sqlite")
+    else create_engine(SQLALCHEMY_DATABASE_URL)
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
